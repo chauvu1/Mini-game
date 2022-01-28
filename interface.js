dfa03eb53e9a55b11413869e6dce94ff6073205e
Chau Vu <cvu1@uw.edu>
2022-01-28 00:57:40 -0800

    Updated interface

--- interface.js
+++ interface.js
@@ -4,7 +4,7 @@ class Interface {
         this.bunsheet = ASSET_MANAGER.getAsset("./sprites/bun_interface.png");
         this.backsheet = ASSET_MANAGER.getAsset("./sprites/back_interface.png");
         this.animation =  new Animator(this.bunsheet, 0, 0, 96, 116, 2, 1, 0, false, true); 
-        this.background = new Animator(this.backsheet, 0, 0, 145, 28, 1, 1, 0, false, true);
+        this.background = new Animator(this.backsheet, 0, 0, 134, 37, 1, 1, 0, false, true);
     }
    
     update () {
@@ -13,7 +13,8 @@ class Interface {
     
     draw(ctx) {
         // this.animation.drawFrame
-        this.background.drawFrame(this.game.clockTick, ctx, this.x + 150, this.y + 30, 5);
+        ctx.drawImage(this.backsheet, 0, 43, 199, 46, this.x, this.y + 10, 144 *7, 52*7);
+        this.background.drawFrame(this.game.clockTick, ctx, this.x + 150, this.y + 22, 5);
         this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
     };
 
